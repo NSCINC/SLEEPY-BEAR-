9e18211944cd8d26ecb73b47dae866d9a6f3ac1d
nsc i.o <158188835+NSCINC@users.noreply.github.com>
2024-05-21 10:38:42 -0300

    Update ponte.php

--- ponte.php
+++ ponte.php
@@ -1,1 +1,15 @@
+<?php
+session_start();
+
+// Verifica se a autenticação foi bem-sucedida (você pode substituir esta condição pela sua lógica real)
+if (isset($_SESSION['authenticated']) && $_SESSION['authenticated'] === true) {
+    // Redireciona para o arquivo de login
+    header("Location: login.php");
+    exit; // Termina o script para garantir que o redirecionamento seja executado
+} else {
+    // Se não estiver autenticado, redireciona de volta para o arquivo de autenticação
+    header("Location: authentication.html");
+    exit; // Termina o script para garantir que o redirecionamento seja executado
+}
+?>
 
